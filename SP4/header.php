f5045412c0e3112a0607e019d714ea41f1f37e57
Tyaaa-aa <69957513+Tyaaa-aa@users.noreply.github.com>
2021-11-12 04:07:24 +0800

    Removed Redundant Files

--- SP4/header.php
+++
@@ -1,9 +0,0 @@
-<header>
-    <ul class="nav">
-        <li><a href="#">Page 1</a></li>
-        <li><a href="#">Page 2</a></li>
-        <li><a href="#">Login</a></li>
-        <li><a href="#">Logout</a></li>
-    </ul>
-    <ion-icon name="menu" class="menu-btn"></ion-icon>
-</header>
